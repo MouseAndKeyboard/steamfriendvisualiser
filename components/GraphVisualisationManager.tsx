3f34d65241442e140e97ba7e506221f69ef63fc5
Michael Nef <michael@nef.net.au>
2021-06-29 20:25:24 +0800

    figured out how to render graph

--- components/GraphVisualisationManager.tsx
+++ components/GraphVisualisationManager.tsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from 'react'
+
 import GraphVisualisation from './Visualisations/GraphVisualisation'
 
 const GraphVisualisationManager = () => {
-    return (<GraphVisualisation/>);
+    const [vertices, setVertices] = useState<steamUser[]>([]);
+    const [edges, setEdges] = useState<relationship[]>([]);
+
+    useEffect(() => {
+        const initialVertexData = [{
+            id: "76561198026645639"
+        }, {
+            id: "76561198779890302"
+        }];
+        setVertices(initialVertexData);
+
+        const initialEdgeData = [{
+            source: "76561198026645639",
+            target: "76561198779890302",
+            note: "friend"
+        }];
+        setEdges(initialEdgeData);
+
+    }, [])
+
+
+    return (<GraphVisualisation vertices={vertices} edges={edges} width={1000} height={1000} />);
 }
 
 export default GraphVisualisationManager;
+
+export interface steamUser {
+    id: String;
+}
+
+export interface relationship {
+    source: String;
+    target: String;
+    note: String;
+}
--- components/Visualisations/GraphVisualisation.tsx
+++ components/Visualisations/GraphVisualisation.tsx
@@ -1,11 +1,79 @@
+import * as d3 from 'd3'
+import { useD3 } from '../../hooks/useD3'
+
 import styles from "./GraphVisualisation.module.css"
 
-const GraphVisualisation = (props) => {
-    return (<svg className={styles.mainView}>
 
+const GraphVisualisation = ({vertices, edges, width, height}) => {
+    const ref = useD3(
+        (svg) => {
+            const links = edges.map(d => Object.create(d));
+            const nodes = vertices.map(d => Object.create(d));
 
-    </svg>);
+            const simulation = d3.forceSimulation(nodes)
+                                 .force('link', d3.forceLink(links).id(d => d.id))
+                                 .force('charge', d3.forceManyBody().strength(-1000))
+                                 .force('center', d3.forceCenter(width / 2, height / 2))
 
+            const link = svg.append('g')
+                            .attr('stroke', '#fff')
+                            .attr('stroke-opacity', 0.6)
+                            .selectAll('line')
+                            .data(links)
+                            .join('line')
+                            .attr('stroke-width', 2)
+
+            const node = svg.append('g')
+                            .attr('stroke', '#fff')
+                            .attr('stroke-width', 1.5)
+                            .selectAll('circle')
+                            .data(nodes)
+                            .join('circle')
+                            .attr('r', 20)
+                            .attr('fill', '#aaa')
+                            .call(drag(simulation))
+
+            node.append('title')
+                            .text(d => d.id);
+
+            simulation.on('tick', () => {
+                link.attr('x1', d => d.source.x)
+                    .attr('y1', d => d.source.y)
+                    .attr('x2', d => d.target.x)
+                    .attr('y2', d => d.target.y);
+
+                node.attr('cx', d => d.x)
+                    .attr('cy', d => d.y);
+            });
+        },
+        [vertices.length, edges.length]
+    );
+
+    const drag = simulation => {
+        function dragstarted(event) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            event.subject.fx = event.subject.x;
+            event.subject.fy = event.subject.y;
+        }
+
+        function dragged(event) {
+            event.subject.fx = event.x;
+            event.subject.fy = event.y;
+        }
+
+        function dragended(event) {
+            if (!event.active) simulation.alphaTarget(0);
+            event.subject.fx = null;
+            event.subject.fy = null;
+        }
+
+        return d3.drag()
+                 .on("start", dragstarted)
+                 .on("drag", dragged)
+                 .on("end", dragended);
+}
+
+    return (<svg className={styles.mainView} ref={ref}></svg>);
 }
 
 export default GraphVisualisation;
