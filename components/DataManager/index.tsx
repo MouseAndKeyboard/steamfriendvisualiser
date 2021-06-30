15fb50e2718ec70c1ab9471f0cf9bbfb16fc43b0
Michael Nef <michael@nef.net.au>
2021-06-30 09:37:24 +0800

    Added instructions

--- components/DataManager/index.tsx
+++ components/DataManager/index.tsx
@@ -2,8 +2,15 @@ import styles from "./index.module.css"
 
 const DataManager = () => {
     return (<div className={styles.main}>
-        <h3>Friend Explorer</h3>
-        <p>The quick brown fox jumped over the lazy dog!1</p>
+        <h2>Steam Friend Explorer</h2>
+        <p>Visualise and interact with your Steam friend network.</p>
+        <br />
+        <h3>Controls</h3>
+        <ul>
+            <li className={styles.listElem}><code>Double Click</code> to add that node's friends to the graph.</li>
+            <li className={styles.listElem}><code>Click and Drag</code> to move nodes around.</li>
+            <li className={styles.listElem}><code>Mouse over</code> to view the steamid of that user.</li>
+        </ul>
     </div>);
 }
 
