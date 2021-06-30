e1dfdaf122059aadf3c1c8ca849cc80780f21ed6
Michael Nef <michael@nef.net.au>
2021-06-30 12:20:22 +0800

    Added better styling for button

--- components/App/index.tsx
+++ components/App/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import GraphVisualisationManager from '../GraphVisualisationManager';
 import DataManager from '../DataManager';
 
 import styles from './index.module.css';
 
+const GraphContext = createContext();
+
 const app = () => {
     return (
         <div className={styles.main}>
-            <DataManager />
+            <DataManager className={styles.infopanel} />
             <GraphVisualisationManager />
         </div>
     );
--- components/DataManager/index.tsx
+++ components/DataManager/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styles from './index.module.css';
 
 const DataManager = () => {
+    const [userSteamid, setUserSteamid] = useState('');
+
+    const handleChange = (e) => {
+        setUserSteamid(e.target.value);
+    };
+
+    const addSteamid = useCallback(
+        () => {
+
+        },
+        [userSteamid]
+    )
+
     return (<div className={styles.main}>
         <h1>Steam Friend Explorer</h1>
         <p>Visualise and interact with your Steam friend network.</p>
@@ -11,12 +24,12 @@ const DataManager = () => {
             Enter your <a href="https://steamid.io/">SteamID64</a> (<code>76561198156591174</code>) and press {"\"Add\""}.
         </p>
 
-        <label htmlFor="userId">Your SteamID</label>
-        <br />
-        <input id="userIdInput" name="userId" value="" placeholder="76561198156591174" />
-        <button>Add</button>
-        <p></p>
-        <br />
+        <div className={styles.customfield}>
+            <label htmlFor="userId">Your SteamID</label>
+            <br />
+            <input id="userIdInput" name="userId" value={userSteamid} onChange={handleChange} placeholder="76561198156591174" />
+            <button>Add</button>
+        </div>
         <h2>Controls</h2>
         <ul>
             <li className={styles.listElem}><code>Double Click</code> to add that node{'\''}s friends to the graph.</li>
